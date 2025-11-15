61c2651f3ed15d3e363307f326e52a5bfccf2f2b
Нгаргондо Тиджани <tyuiuTidzhanin@outlook.com>
2025-11-15 23:44:15 +0500

    Task7

--- Tyuiu.Tidzhanin.Sprint4.Task6.V6.Test/DataServiceTest.cs
+++ Tyuiu.Tidzhanin.Sprint4.Task6.V6.Test/DataServiceTest.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.Tidzhanin.Sprint4.Task6.V6.Test
             string[] array = { "Борис", "Анна", "Михаил", "Ирина", "Сергей", "Татьяна", "Олег" };
             string[] result = ds.Calculate(array);
 
-            string[] expected = { "Борис", "Ирина", "Олег" };
+            string[] expected = { "Борис", "Ирина" };
 
             CollectionAssert.AreEqual(expected, result);
         }
